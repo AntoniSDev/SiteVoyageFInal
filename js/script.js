02a6fd0fcb1cfa1c44e18e7747e46f8213b9a5a2
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-01-19 17:40:29 +0100

    Slider W3C en cours

--- js/script.js
+++ js/script.js
@@ -6,3 +6,33 @@
         menuHamburger.addEventListener('click',()=>{
         navLinks.classList.toggle('mobile-menu')
         })
+
+
+        
+
+
+
+
+
+
+
+
+
+//script slide show w3c
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// fin script slide show w3c
