f05801b6b5bb047c24bb53f59758280ef6db334b
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-01-19 23:45:05 +0100

    Resize Img+Caraousel

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
 // fin script slide show w3c
